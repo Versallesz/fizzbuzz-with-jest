74b560dfb075c64caf5bfadb7280d218bb242eee
Versalles <toma.galvez@duocuc.cl>
2021-06-20 22:42:42 -0400

    agregados archivos de fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,38 @@
+function fizzbuzz(num){
+    
+    if(typeof num !== 'number'){
+        return "Error! the argument must be a number";
+        
+    }
+
+    const divisible = (divisor, num) => num % divisor === 0;
+    
+
+    if(num === 0){
+        return 0;
+    }
+
+    if(divisible(3, num) && divisible(5, num)){
+        return "fizzbuzz";
+    }
+
+    if(divisible(3, num)){
+        return "fizz";
+    }
+
+    if(divisible(5, num)){
+        return "buzz";
+    }
+    return num;
+}
+
+function printNumbers(num){
+    for (let i = 0; i < num; i++){ 
+        console.log(`${i}: ${fizzbuzz(i)}`);
+    }
+}
+
+printNumbers(18);
+
+
+module.exports = fizzbuzz;
